cfebd22e1109192e542f27a026782ef162dfbb01
Martin42 <martinarmada@ua.pt>
2024-01-12 17:06:48 +0000

    .

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <>
       <div className="App">
-        <h2>JEOPARDY</h2>
+        <h2>XMAS JEOPARDY 2023</h2>
       </div>
       <PlayerScore />
       <MainQuestions />
